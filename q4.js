76f6f2338953be1163c18ed3b08fd471502638d8
mohaddeseht <103627084+mohaddeseht@users.noreply.github.com>
2022-05-04 23:27:54 +0430

    Delete q4.js

--- q4.js
+++
@@ -1,7 +0,0 @@
-const time = new Date();
-function getTime(time) {
-
-
-    return time.toLocaleString("en-US", { hour: "numeric", minute: "numeric", hour12: true });
-}
-console.log(getTime(time));
