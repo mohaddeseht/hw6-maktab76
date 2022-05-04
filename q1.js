4015805745f7e14c915d3063ee5b3f53f02f717c
mohaddeseht <103627084+mohaddeseht@users.noreply.github.com>
2022-05-04 23:23:39 +0430

    Delete q1.js

--- q1.js
+++
@@ -1,8 +0,0 @@
-function accumulator(array) {
-    let tempArray = [];
-    array.reduce((acc, v, i) => {
-        return tempArray[i] = acc + v;
-    }, 0);
-    return tempArray;
-}
-console.log(accumulator([1, 5, 7]));
